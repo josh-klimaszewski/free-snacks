60cb5a616a057104684efa04426f5d091db84267
Josh Klimaszewski <joshklimaszewski@Joshs-MacBook-Pro.local>
2018-05-16 10:01:46 -0400

    about page markup

--- src/pages/AboutPage/AboutPage.js
+++ src/pages/AboutPage/AboutPage.js
@@ -3,7 +3,132 @@ import './style.css';
 
 const AboutPage = () => {
     return (
-        <div className="about-page">About</div> 
+        <div className="about-page">
+            <div className="item1">
+                (free snacks) is a series of video conversations about food, 
+                memory and identity conducted in public spaces around Pittsburgh.
+            </div>
+            <div className="item2">
+                <div>
+                Everybody seems so young. And everybody’s talking 
+                about restaurants. If there are scholars who hope 
+                to study how a vibrant food culture can help radically 
+                transform an American city, the time to do that is right 
+                now, in real time, in the place that gave us Heinz ketchup… 
+                No one can pinpoint whether it was the artists or techies 
+                or chefs who got the revitalization rolling.
+                </div>
+                <div>
+                -Jeff Gordinier, March 15, 2016, The New York Times
+                </div>
+            </div>
+            <div className="item3">
+                <div>
+                I keep seeing versions of the same story about Pittsburgh’s changing restaurant 
+                and sustainable agriculture scene: young, recent transplants 
+                to Pittsburgh with tech money and a love of the arts are innovating food 
+                in a city that subsisted only on meat, potatoes and ketchup for decades. 
+                Taking a drive down Penn Avenue, I can see the uneven benefits of Pittsburgh’s 
+                tech boom/arts renaissance/food revolution. Affordable housing options are dwindling, 
+                but Whole Foods needs more space.
+                </div>
+                <div>
+                (free snacks) is a public history project that documents and celebrates the differences in taste, 
+                identity and memory among the city’s residents.  Everybody eats–not just foodies, techies and artists.  
+                The narrative of a changing city and its foodscape should include the experiences of all residents. 
+                This project is a step towards a more democratic version of Pittsburgh’s story. The thoughts and memories 
+                of the 77 narrators included in this project only tap the surface of the range and complexity of voices 
+                in the city. Excerpts of these video conversations will be screened at The Livermore on Wednesday, 
+                August 31 alongside prints by Madalyn Hochendoner and designs by Stephanie Hall. Transcripts of all 
+                (free snacks) conversations are archived on this website.
+                </div>
+            </div>
+            <div className="item4">
+                <div>
+                <span>Clara Gamalski</span> coordinated (free snacks) for her master’s thesis in the Food Studies 
+                program at Chatham University. Originally from the Detroit area, Clara moved to Pittsburgh in 2014 
+                with her partner, Josh, and her dog, Detective Lenny Briscoe. She is a researcher and the operations 
+                manager at Conflict Kitchen. She is unspeakably grateful to the narrators of this project for sharing 
+                their stories, as well as the community of friends, family and mentors who gave their time, expertise 
+                and creativity to this project.
+                </div>
+                <div>
+                The <a href="http://falk.chatham.edu/mafs/">Master of Arts in Food Studies</a> program at Chatham University provides 
+                a holistic, interdisciplinary approach to food systems, from production to consumption, focusing on issues of sustainability, systems, 
+                and equity.  Graduates acquire experiential and intellectual knowledge in agriculture, food cultures, 
+                cuisines, cultural representation, and myriad other skills to help them address the challenges of the 
+                contemporary landscape of food and agriculture.  From meat production to media literacy, from pest management 
+                to global coffee production, the program engages in local and global food systems with an emphasis on making 
+                it accessible and better for all participants.
+                </div>
+            </div>
+            <div className="item5">
+                <div>
+                Collaborators</div>
+                <div>
+                <span>Madalyn Hochendoner</span> is an interdisciplinary artist, poet & cook living in Pittsburgh.  Her work seeks 
+                to express an altered landscape of memory & emotion through the physical parallels of landscape & food.  Her recent 
+                work includes a series of lithographs & poems that reclaim landscape through a feminist lens redefining & reclaiming 
+                ideas of “ownership,” “sexy,” & “luxury.”  She considers all her work to be a part of an ongoing investigation 
+                into/expansion of the art of still lifes. Digital archives of her prints for this show are available in the exhibit section.
+                </div>
+                <div>
+                <span>Emeran Irby</span> is a master’s candidate in Food Studies at Chatham University. 
+                Her areas of interests include: lady brewers, Appalachian foodways, and donuts. 
+                She currently lives in Pittsburgh where she scoops ice cream at Millie’s and lives happily with her cat Nelson Mandela.
+                </div>
+                <div>
+                <span>Stephanie Hall</span> is a recent Pittsburgh transplant and aspiring artist whose lifetime food service 
+                experience spans from organic farming to cocktail mixing. She’s also lived in a few food deserts. 
+                Her interests include bookmaking, fruit and snail mail.
+                </div>
+                <div>
+                <span>Josh Klimaszewski</span> is a Michigan-born artist, musician and chef. His interests include making pasta, 
+                playing tricks on his dog and taking apart cassette tapes. He currently works at Conflict Kitchen in Oakland and 
+                lives in Lawrenceville. He’s deep-fried his glasses twice, but he still wears them.
+                </div>
+            </div>
+            <div className="item6">
+                <div>Advisors</div>
+                <div><span>Alice Julier</span> is the Program Director of and Associate Professor in the Food Studies Program at the 
+                Falk School of Sustainability at Chatham University. Alice Julier is a sociologist who has been teaching and writing 
+                about inequality, food, and everyday life for two decades. Prior research include the feminist women’s health 
+                organizations, gender, race, and leadership in the US Civil Rights Movement, and critical social theory. These 
+                interests center on how people navigate practice and politics, activism and everyday life.</div>
+                <div><span>Sherrie Flick</span> is a fiction writer, food writer, and freelance writer and copy editor living in 
+                Pittsburgh, Pennsylvania. Her book publications include the flash fiction chapbook I Call This Flirting, Reconsidering 
+                Happiness: A Novel, and the short story collection Whiskey, Etc. (Queen’s Ferry Press, 2016.) Her food writing appears 
+                in Ploughshares, Pittsburgh Quarterly, The Pittsburgh Post-Gazette, and NEXTPittsburgh as well as the anthology Fried 
+                Walleye and Cherry Pie: Midwestern Writers on Food. Over the years, Sherrie has led interdisciplinary writing workshops 
+                in many arts institutions, including Carnegie Museum of Art and Silver Eye Center for Photography.  She lives on the South 
+                Side Slopes of Pittsburgh with her husband, Rick Schweikert. http://sherrieflick.com/</div>
+                <div><span>David Bernabo </span> is an active filmmaker, musician, dancer, and visual artist. David currently performs and 
+                composes for the band Host Skull, dances and composes for Maree ReMalia | merrygogo and his own variable dance company, MODULES, 
+                and acts as the Arts and Culture Editor of The Glassblock. He curates and produces work for the Ongoing Box imprint and co-curates 
+                the Lightlab Performance Series with choreographer Taylor Knight. David‘s recent projects include a four-film documentary series 
+                called Food Systems, an art exhibit at SPACE Gallery with Blaine Siegel, and a recent evening-length dance work at New Hazlett 
+                Theater for seven movers and one musician called The Reduction. www.davidbernabo.com</div>
+            </div>
+            <div className="item7">
+                <div>Interviewers</div>
+                <div><span>Hillary Bedeian</span>, former Pittsburgher, is an educator living in New Orleans, Louisiana. She enjoys eating Oscar Mayer 
+                baloney and has a soft spot for hard cheeses.</div>
+                <div><span>Eleanor Gamalski</span> is the Community Organizer for <a href="https://www.detroitjewsforjustice.org/">Detroit Jews for 
+                Justice</a>, a grassroots organization which mobilizes Jewish community to support racial and economic justice causes. She attended 
+                the University of Michigan where she minored in Urban Studies  and majored in International Studies with a focus in Haitian history.  
+                She received her MPA from Brown University in 2016. She lives in Detroit, and graciously spent a weekend in Pittsburgh conducting 
+                interviews with her sister.</div>
+                <div><span>Gianna Fazioli</span> is a recent graduate of the Chatham MAFS program and the current Director of Shady Side Academy’s 
+                farm program. Gianna has experience and knowledge working in the local and sustainable food systems of Pittsburgh and teaching kids 
+                of all ages about them along the way. When she isn’t managing the Shady Side Academy farm program, she is leading the farmers market 
+                sales as a farmers market coordinator at Wigle Whiskey. For more information on the Shady Side Academy Farms, connect on 
+                <a href="https://m.facebook.com/foxchapelfarmmarket/">Facebook</a> or email gfazioli@shadysideacademy.org.</div>
+                <div><span>Jenson Leonard</span> is a Pittsburgh based poet, performance artist and digital media artist. A recent graduate of 
+                Duquesne University, he hopes to apply to graduate school this year to elevate his <a href="http://Instagram.com/coryintheabyss">craft</a>
+                 as far as it might go.</div>
+                <div>Additional interview support provided by Sondra Hart. Event photography by Eme Truechild Pfleegor. Web support by Jon Werber.</div>
+            </div>
+        </div> 
        );
 }
 
